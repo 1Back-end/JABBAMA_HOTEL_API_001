8780e60de41cc6a1cf8baa74000c5a6540d55655
Abousiah <hamadjoda.ia@gmail.com>
2026-05-12 05:29:28 +0100

    recup

--- app/Enums/OrderMenuRestaurantItemStatus.php
+++ app/Enums/OrderMenuRestaurantItemStatus.php
@@ -34,7 +34,7 @@ enum OrderMenuRestaurantItemStatus: string
     {
         return match ($this) {
             self::PENDING => 'En attente',
-            self::DELIVERED => 'Servie totalement',
+            self::DELIVERED => 'Servie',
             self::NOT_DELIVERED => 'Non servie',
             self::PARTIAL_DELIVERED => 'Servie partiellement',
             self::DELIVERED_IN_PREPARATION => 'Prêt pour service',
